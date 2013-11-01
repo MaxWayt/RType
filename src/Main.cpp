2f006b63035671cfb3f238ec8f9ce99decc3fe4a
Max Fest <fest@dragonwar.fr>
2013-11-01 22:06:09 +0100

    Main : check config load

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,7 +11,11 @@
 int main(int ac, char** av)
 {
     sConfig->SetConfigFile(DEFAULT_CONFIG_FILE);
-    sConfig->LoadConfig();
+    if (!sConfig->LoadConfig())
+    {
+        std::cerr << "Fail to load config `" << DEFAULT_CONFIG_FILE << "`" << std::endl;
+        return 1;
+    }
     sConfig->DumpConfig();
 
     Server server;
