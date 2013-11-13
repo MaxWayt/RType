522bd67760fc6426b164ec33c408797a4b7e07c7
newincpp <newincpp@gmail.com>
2013-11-13 00:29:08 +0000

    fuck you git ... seriously

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -4,11 +4,11 @@
 int main(int ac, char** av) {
     DisplayManager* d;
 
-    /*if (ac > 2) {
+    if (ac > 2) {
         d = new DisplayManager(std::atoi(av[1]), std::atoi(av[2]));
-    } else {*/
-        d = new DisplayManager(140, 90, false);//1920, 1080, false);
-    //}
+    } else {
+        d = new DisplayManager(1920, 1080, false);
+    }
     d->run();
     return EXIT_SUCCESS;
 }
