413098f78dbc78d3c3a5eb3eab525da2159a775d
Max Fest <fest@dragonwar.fr>
2013-11-20 23:48:47 +0100

    Move shoot pattern to monster

--- src/client/ShootPattern.hpp
+++ src/client/ShootPattern.hpp
@@ -14,14 +14,14 @@
 
 class ShootPatternDefault : public DamnCute::APattern {
     public:
-        explicit ShootPatternDefault(glm::vec2 v, bool reverse = false) : _v(v), _reverse(reverse) {
+        explicit ShootPatternDefault(glm::vec2 v) : _v(v) {
             initialize();
         }
 
         virtual ~ShootPatternDefault() = default;
         virtual void initialize() {
 
-            _m = glm::translate(glm::mat4(), glm::vec3(_reverse ? 15 : -15, 0, 0));
+            _m = glm::translate(glm::mat4(), glm::vec3(-15, 0, 0));
             _x = new DamnCute::Path(_m, TIME_SEPARATOR, DamnCute::Bullet(glm::vec2(0, 0.0f), 0, 150), "../resources/pink-bullet.tga");
             _x->setPhysicBulletModelId(5);
             addPath(_x);
@@ -31,24 +31,23 @@ class ShootPatternDefault : public DamnCute::APattern {
         DamnCute::Path *_x;
         glm::vec2 _v;
         glm::mat4 _m;
-        bool _reverse;
 };
 
 class ShootPatternLevel1 : public DamnCute::APattern {
     public:
-        explicit ShootPatternLevel1(glm::vec2 v, bool reverse = false) : _v(v), _reverse(reverse) {
+        explicit ShootPatternLevel1(glm::vec2 v) : _v(v) {
             initialize();
         }
 
         virtual ~ShootPatternLevel1() = default;
         virtual void initialize() {
 
-            _m = glm::translate(glm::mat4(), glm::vec3(_reverse ? 15 : -15, 2, 0));
+            _m = glm::translate(glm::mat4(), glm::vec3(-15, 2, 0));
             _y = new DamnCute::Path(_m, TIME_SEPARATOR, DamnCute::Bullet(glm::vec2(0, 0.0f), 0, 150), "../resources/pink-bullet.tga");
             _y->setPhysicBulletModelId(5);
             addPath(_y);
 
-            _m = glm::translate(glm::mat4(), glm::vec3(_reverse ? 15 : -15, -2, 0));
+            _m = glm::translate(glm::mat4(), glm::vec3(-15, -2, 0));
             _z = new DamnCute::Path(_m, TIME_SEPARATOR, DamnCute::Bullet(glm::vec2(0, 0.0f), 0, 150), "../resources/pink-bullet.tga");
             _z->setPhysicBulletModelId(5);
             addPath(_z);
@@ -59,29 +58,28 @@ class ShootPatternLevel1 : public DamnCute::APattern {
         DamnCute::Path *_z;
         glm::vec2 _v;
         glm::mat4 _m;
-        bool _reverse;
 };
 
 class ShootPatternLevel2 : public DamnCute::APattern {
     public:
-        explicit ShootPatternLevel2(glm::vec2 v, bool reverse = false) : _v(v), _reverse(reverse) {
+        explicit ShootPatternLevel2(glm::vec2 v) : _v(v) {
             initialize();
         }
 
         virtual ~ShootPatternLevel2() = default;
         virtual void initialize() {
 
-            _m = glm::translate(glm::mat4(), glm::vec3(_reverse ? 15 : -15, 0, 0));
+            _m = glm::translate(glm::mat4(), glm::vec3(-15, 0, 0));
             _x = new DamnCute::Path(_m, TIME_SEPARATOR, DamnCute::Bullet(glm::vec2(0, 0.0f), 0, 150), "../resources/pink-bullet.tga");
             _x->setPhysicBulletModelId(5);
             addPath(_x);
 
-            _m = glm::translate(glm::mat4(), glm::vec3(_reverse ? 15 : -15, 4, 0));
+            _m = glm::translate(glm::mat4(), glm::vec3(-15, 4, 0));
             _y = new DamnCute::Path(_m, TIME_SEPARATOR, DamnCute::Bullet(glm::vec2(0, 0.0f), 0, 150), "../resources/pink-bullet.tga");
             _y->setPhysicBulletModelId(5);
             addPath(_y);
 
-            _m = glm::translate(glm::mat4(), glm::vec3(_reverse ? 15 : -15, -4, 0));
+            _m = glm::translate(glm::mat4(), glm::vec3(-15, -4, 0));
             _z = new DamnCute::Path(_m, TIME_SEPARATOR, DamnCute::Bullet(glm::vec2(0, 0.0f), 0, 150), "../resources/pink-bullet.tga");
             _z->setPhysicBulletModelId(5);
             addPath(_z);
@@ -93,7 +91,6 @@ class ShootPatternLevel2 : public DamnCute::APattern {
         DamnCute::Path *_z;
         glm::vec2 _v;
         glm::mat4 _m;
-        bool _reverse;
 };
 
 #endif /* !SHOOTPATTERN_H_ */
